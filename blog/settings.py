766c1eb27ecec45a8ad8140209996cf44ab0fded
rezarex <marvin.wekesa@gmail.com>
2023-03-26 12:54:32 +0300

    auth and blog

--- blog/settings.py
+++ blog/settings.py
@@ -95,14 +95,25 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.environ.get('DB_ENGINE'),
+#         "NAME": os.environ.get('DB_NAME'),
+#         "USER": os.environ.get('DB_USER'),
+#         "PASSWORD":os.environ.get('DB_PASSWORD'),
+#         "HOST":os.environ.get('DB_HOST'),
+#         "PORT": os.environ.get('DB_PORT')
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get('DB_ENGINE'),
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD":os.environ.get('DB_PASSWORD'),
-        "HOST":os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
