0194022a84a259bddf4cba5e1b00e3d123918099
rezarex <marvin.wekesa@gmail.com>
2023-03-26 16:18:45 +0300

    edit yaml file

--- blog/settings.py
+++ blog/settings.py
@@ -95,23 +95,23 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": 'django.db.backends.postgresql',
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD":os.environ.get('DB_PASSWORD'),
-        "HOST":os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT')
-    }
-}
-
 # DATABASES = {
-#   'default' :{
-#     'ENGINE':'django.db.backends.sqlite3',
-#     'NAME':BASE_DIR / 'db.sqlite3',
-#   }
+#     "default": {
+#         "ENGINE": 'django.db.backends.postgresql',
+#         "NAME": os.environ.get('DB_NAME'),
+#         "USER": os.environ.get('DB_USER'),
+#         "PASSWORD":os.environ.get('DB_PASSWORD'),
+#         "HOST":os.environ.get('DB_HOST'),
+#         "PORT": os.environ.get('DB_PORT')
+#     }
 # }
+
+DATABASES = {
+  'default' :{
+    'ENGINE':'django.db.backends.sqlite3',
+    'NAME':BASE_DIR / 'db.sqlite3',
+  }
+}
 
 
 # Password validation
